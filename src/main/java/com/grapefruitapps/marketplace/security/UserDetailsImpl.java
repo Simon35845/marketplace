48526377f74af269d97bf8e887d99695b3d2c47e
Admin5195 <DrakeKingman1991@gmail.com>
2026-03-15 19:11:16 +0300

    commit 6.13 - Added UserDetailsImpl and UserDetailsServiceImpl

---
+++ src/main/java/com/grapefruitapps/marketplace/security/UserDetailsImpl.java
@@ -0,0 +1,39 @@
+package com.grapefruitapps.marketplace.security;
+
+import com.grapefruitapps.marketplace.user.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/grapefruitapps/marketplace/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.grapefruitapps.marketplace.security;
+
+import com.grapefruitapps.marketplace.user.entity.User;
+import com.grapefruitapps.marketplace.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Get user by username: {}", username);
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("Not found user by username: " + username)
+        );
+        log.info("Found user with username: {}", username);
+        return new UserDetailsImpl(user);
+    }
+}
