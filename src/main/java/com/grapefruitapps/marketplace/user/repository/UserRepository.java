550fd632bb4fc007f35f139c129a8f315fbf676f
Admin5195 <DrakeKingman1991@gmail.com>
2026-03-15 19:05:50 +0300

    commit 6.12 - update repositories

--- src/main/java/com/grapefruitapps/marketplace/user/repository/UserRepository.java
+++ src/main/java/com/grapefruitapps/marketplace/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.grapefruitapps.marketplace.user;
+package com.grapefruitapps.marketplace.user.repository;
 
+import com.grapefruitapps.marketplace.user.entity.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,28 +8,27 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
     @Query("""
             select u from User u
             where (:name is null or u.name = :name)
             and (:phone is null or u.phone = :phone)
             and (:email is null or u.email = :email)
-            and (:country is null or u.address.country = :country)
-            and (:city is null or u.address.city = :city)
-            and (:street is null or u.address.street = :street)
-            and (:house is null or u.address.house = :house)
             order by u.id
             """)
     List<User> searchAllByFilter(
             @Param("name") String name,
             @Param("phone") String phone,
             @Param("email") String email,
-            @Param("country") String country,
-            @Param("city") String city,
-            @Param("street") String street,
-            @Param("house") String house,
             Pageable pageable
     );
 }
